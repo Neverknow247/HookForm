1c6a2cda8c2535f8c92f0dac6b67844ab33dc202
Nathan Bell <nathan.bell421@icloud.com>
2021-01-14 18:28:01 -0600

    add validations

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -9,6 +9,7 @@ const UserForm = (props) => {
     const [cfmPassword, setcfmPassword] = useState("");
 
     const createUser = (e) => {
+        setFirstName("");
         e.preventDefault();
         const newUser = { firstName, lastName, email, password, cfmPassword };
         console.log("Welcome", newUser);
@@ -19,23 +20,30 @@ const UserForm = (props) => {
             <form onSubmit={createUser}>
                 <div>
                     <label>First Name: </label>
-                    <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+                    <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} /><br></br>
+                    <span className="error">{firstName.length >0 && firstName.length <2 ? "must be 2 long": ""}</span>
                 </div>
                 <div>
                     <label>Last Name: </label>
-                    <input type="text" onChange={(e) => setLastName(e.target.value)} vlaue={lastName} />
+                    <input type="text" onChange={(e) => setLastName(e.target.value)} vlaue={lastName} /><br></br>
+                    <span className="error">{lastName.length >0 && lastName.length <2 ? "must be 2 long": ""}</span>
                 </div>
                 <div>
                     <label>Email: </label>
-                    <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
+                    <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} /><br></br>
+                    <span className="error">{email.length >0 && email.length <5 ? "must be 5 long": ""}</span>
                 </div>
                 <div>
                     <label>Password: </label>
-                    <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                    <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} /><br></br>
+                    <span className="error">{password.length >0 && password.length <8 ? "must be 8 long": ""}</span><br></br>
+                    <span className="error">{password != cfmPassword ? "passwords must match":""}</span>
                 </div>
                 <div>
                     <label>Confirm Password: </label>
-                    <input type="password" onChange={(e) => setcfmPassword(e.target.value)} value={cfmPassword} />
+                    <input type="password" onChange={(e) => setcfmPassword(e.target.value)} value={cfmPassword} /><br></br>
+                    <span className="error">{cfmPassword.length >0 && cfmPassword.length <8 ? "must be 8 long ": ""}</span><br></br>
+                    <span className="error">{password != cfmPassword ? "passwords must match":""}</span>
                 </div>
                 <input type="submit" value="Create User" />
             </form>
